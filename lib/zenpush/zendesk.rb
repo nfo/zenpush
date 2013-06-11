0939dbd5b805beeae8b5c9661d034d95fa234ae1
Alex Wolfe <alexkwolfe@gmail.com>
2013-06-11 13:29:28 -0500

    Finding a forum by name is scoped to the category. Issue #8.

--- lib/zenpush/zendesk.rb
+++ lib/zenpush/zendesk.rb
@@ -102,7 +102,7 @@ module ZenPush
     def find_forum(category_name, forum_name, options = {})
       category = self.find_category(category_name, options)
       if category
-        self.forums.detect {|f| f['name'] == forum_name}
+        self.forums.detect {|f| f['category_id'] == category['id'] && f['name'] == forum_name }
       end
     end
 
@@ -110,7 +110,7 @@ module ZenPush
     def find_or_create_forum(category_name, forum_name, options={ })
       category = self.find_or_create_category(category_name, options)
       if category
-        self.forums.detect { |f| f['name'] == forum_name } || post_forum(category['id'], forum_name)
+        self.forums.detect { |f| f['category_id'] == category['id'] && f['name'] == forum_name } || post_forum(category['id'], forum_name)
       end
     end
 
